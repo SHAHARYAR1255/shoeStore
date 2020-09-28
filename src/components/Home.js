f816e3d338471b8c7c7e2715ea53a717ff865d8e
= <shaharyar.malik2000@gmail.com>
2020-09-29 02:31:53 +0500

    home updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,19 +8,18 @@ function Home() {
   const store = useContext(MyContext);
     const keys = Object.keys(store);
     const new_keys = keys.slice(0,2);
+    const param1= new_keys[0];
+    const param2= new_keys[1];
    
   return(
     <>
       <h1 className="store__text">New Collection</h1>
 
       <div className="store">
-        {new_keys.map(key =>{
-        const shoe = store[key];
-        return(
-          <HomeCard key={key} shoe={shoe} />
-        )
-        }
-    )}
+       
+          <HomeCard param={param1} shoe={store[param1]} />
+          <HomeCard param={param2} shoe={store[param2]} />
+
         <Link to="/store" className="btn align-self-center mt-5 mb-3">TO STORE --&gt;</Link>
 
     </div>
--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
-// import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import './Store.css';
 
-function HomeCard({shoe}) {
-
+function HomeCard({shoe, param}) {
    return(
+     <Link key={param} to={`/store/${param}`} >
       <div className="card" key={shoe.name}>
         <img className="card__img"  src={shoe.img} alt="" />
           <div className="card__text">
             <h4>{shoe.name}</h4>
           </div>
       </div>
+      </Link>
         )
         }
 
